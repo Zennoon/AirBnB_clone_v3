b9a5695aa64baf9993a7aa4bcc0938a870d27954
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-05-25 17:35:11 +0300

    Add the /api/v1/stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,36 @@
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+classes = {
+    "amenities": Amenity, 
+    "cities": City, 
+    "places": Place, 
+    "reviews": Review, 
+    "states": State, 
+    "users": User
+}
 
 @app_views.route("/status")
 def status():
     """Route function for /api/v1/status"""
     return (jsonify({"status": "OK"}))
+
+@app_views.route("/stats")
+def stats():
+    """
+    Route function for /api/v1/stats
+    
+    Displays the number of objects of each class
+    """
+    objs = {}
+    for cls in classes.keys():
+        objs[key] = storage.count(classes[cls])
+    return (jsonify(objs))
