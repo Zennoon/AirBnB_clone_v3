461d72622455b35b2464afda58f848323b43e8b9
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-05-26 00:07:59 +0300

    Add PUT method support to update State instances

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-Handles RESTful API actions for the State class 
+Handles RESTful API actions for the State class
 """
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models import storage
 from models.state import State
+
 
 @app_views.route("/states", methods=["GET"])
 def get_states():
@@ -35,9 +36,11 @@ def get_state(state_id):
 def delete_state(state_id):
     """
     Route function of DELETE /api/v1/states/<state_id>
+
+    Deletes a State instance with given ID if it exists
     """
     state = storage.get(State, state_id)
-    if not state:
+    if state is None:
         abort(404)
     state.delete()
     storage.save()
@@ -48,6 +51,8 @@ def delete_state(state_id):
 def create_state():
     """
     Route function of POST /api/v1/states
+
+    Creates a new State instance with attributes from request body
     """
     try:
         dct = request.get_json()
@@ -57,4 +62,26 @@ def create_state():
     if name is None:
         return ("Missing name", 400)
     new_state = State(**dct)
+    new_state.save()
     return (jsonify(new_state.to_dict()), 201)
+
+
+@app_views.route("/states/<state_id>", methods=["PUT"])
+def update_state(state_id):
+    """
+    Route function for PUT /api/v1/states
+
+    Updates a State instance with given ID, if it exists
+    """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    try:
+        dct = request.get_json()
+    except Exception:
+        return ("Not a JSON", 400)
+    for key in dct.keys():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(state, key, dct[key])
+    storage.save()
+    return (jsonify(state.to_dict()), 200)
