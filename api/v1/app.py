990e6ab1440316871ccdd465a227f01b622eb94e
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-05-27 00:10:46 +0300

    Add support for cross-origin requests

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,10 +8,15 @@ Contains:
 import os
 from flask import Flask
 from flask import jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
+CORS(app, resources={
+    r"/*": {"origins": "0.0.0.0"}
+})
+
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
